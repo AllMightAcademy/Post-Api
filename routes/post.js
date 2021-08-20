9a314124afc351736962fde0243f8f5ac50f886d
vivekkushwaha66 <vivekkushwaha66@gmail.com>
2021-08-20 21:37:37 +0530

    added update & delete method

--- routes/post.js
+++ routes/post.js
@@ -21,4 +21,35 @@ router.get("", (req, res, next) => {
 });
 
 
+router.put("", (req, res, next) => {
+    const { id, title, content } = req.body;
+    if (!id) res.status(404).json({ message: `Post not found` });
+
+    Post.findOne({
+        where: {
+            id
+        }
+    })
+        .then(result => result.update({ title, content }))
+        .then(result => result.save())
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(500).json(error));
+});
+
+
+router.delete("/:id", (req, res, next) => {
+    const id = req.params['id'];
+    if (!id) res.status(404).json({ message: `Post not found` });
+    Post.findOne({ where: { id } })
+        .then(result => {
+            if (result)
+                return result.destroy()
+            return res.status(404).json({ message: `Post not found` });
+        })
+        .then(() => res.status(200).json({ message: `Post with id ${id} is deleted successfully` }))
+        .catch(error => res.status(500).json(error));
+
+});
+
+
 module.exports = router;
